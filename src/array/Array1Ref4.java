3b1492e3e7c55ec3eddf96643986d477ec21d558
Jmpak99 <jmpak99@gmail.com>
2025-01-17 09:56:04 +0900

    Array

--- src/array/Array1Ref4.java
+++ src/array/Array1Ref4.java
@@ -3,7 +3,7 @@ package array;
 public class Array1Ref4 {
     public static void main(String[] args) {
         // 배열 생성 간략 버전, 배열 선언와 함께 사용 시 new int[] 생략 가능
-        int[] students = {90, 80, 70, 60, 50}; // 배열 생성과 초기화
+        int[] students = {90, 80, 70, 60, 50, 40, 30}; // 배열 생성과 초기화
 
 
         // 변수 값 사용
